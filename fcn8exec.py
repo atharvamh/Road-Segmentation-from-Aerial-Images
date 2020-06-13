6bd86f124a69d8222e35b1198437c1d7a8f9a739
Atharva <46068146+atharvamh@users.noreply.github.com>
2020-06-13 12:51:36 +0530

    Update fcn8exec.py

--- fcn8exec.py
+++ fcn8exec.py
@@ -1,3 +1,26 @@
 from fcn import *
 from preprocess import *
-from 
+from metrics import *
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+Epochs = 30
+LearningRate = 0.0001
+BatchSize = 2
+
+FCN8model = FCN8(512)
+FCN8model.summary()
+
+chkpoint = ModelCheckpoint('roadseg.h5',monitor='val_acc',save_best_only=True,mode='auto',verbose=1)
+early = EarlyStopping(monitor='val_acc',min_delta=0,patience=10,verbose=1,mode='auto',restore_best_weights=True)
+
+opt = Adam(LearningRate)
+FCN8model.compile(optimizer=opt,loss='binary_crossentropy',metrics=['accuracy',Dice_Coeff,MeanIoU(num_classes=2)])
+
+history = FCN8model.fit(X_train,Y_train,epochs=Epochs,batch_size=BatchSize,callbacks=[chkpoint,early])
+
+FCN8model.save('fcn8_weights.h5')
+
+out = FCN8model.predict(X_test,batch_size=2)
+
+predRoads = squeeze(out,axis=3)
+y_test = squeeze(Y_test,axis=3)
